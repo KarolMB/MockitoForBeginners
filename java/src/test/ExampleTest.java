8d15a5f0fa1e4b05d3f9ea1dcdb3f6dc2f113158
Karol Baran <karol.baran@tieto.com>
2018-03-27 11:08:12 +0200

    Add first example test

---
+++ java/src/test/ExampleTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void example_test() {
+        assertEquals(4, 2 + 2);
+    }
+}
